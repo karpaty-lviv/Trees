95aa7c6d213971df4316e2928e944e191d4c7bf9
Marko Pasternak <pasternak.pn@ucu.edu.ua>
2025-04-30 09:40:42 +0300

    solution to sort binary tree

--- sort_binary_tree_by_levels.py
+++ sort_binary_tree_by_levels.py
@@ -12,4 +12,15 @@ def tree_by_levels(node):
     """Sort tree by levels"""
     if node is None:
         return []
-    
+    result = []
+    stack = [node]
+    for nod in stack:
+        if nod.left is not None:
+            stack.append(nod.left)
+        if nod.right is not None:  
+            stack.append(nod.right)
+    while stack:
+        result.append(stack.pop(0).value)
+    return result
+
+print(tree_by_levels(Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1)))
